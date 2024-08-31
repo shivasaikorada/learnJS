0d6edc766ee07e813e8254e9e7c02815d849bc61
shivasaikorada <myselfkss9911@gmail.com>
2024-08-31 17:03:46 +0530

    Chrome Extension making

--- URL-SAVER/script.js
+++ URL-SAVER/script.js
@@ -1,4 +1,20 @@
-let inputBtn = document.getElementById("input-button")
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-button")
+const ulEl = document.getElementById("ul-el")
+
 inputBtn.addEventListener("click", function(){
-    console.log("Button clicked")
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    printLeads()
 })
+
+
+function printLeads(){
+    let listItems = ""
+    for(let i = 0; i<myLeads.length; i++){
+        listItems += "<li><a target='_blank' href='"+myLeads[i]+"'>"+myLeads[i]+"</a></li>"
+    }
+    ulEl.innerHTML = listItems
+    
+}
